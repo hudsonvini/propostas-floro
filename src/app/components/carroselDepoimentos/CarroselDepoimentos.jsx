a3bdfd31d69e343fa924abac2fcb8c769aa98591
Hudson <hudsonvini26@gmail.com>
2024-09-14 11:54:39 -0300

    add carrosel and style responsive

--- src/app/components/carroselDepoimentos/CarroselDepoimentos.jsx
+++ src/app/components/carroselDepoimentos/CarroselDepoimentos.jsx
@@ -4,7 +4,6 @@ export default function CarroselDepoimentos() {
     return(
         <section className={style.container}>
             {/* Title */}
-            <span className={style.subTitle}>É o governo do povo</span>
             <h2 className={style.title}>A População está com ele!</h2>
 
             {/* Roll Container */}
@@ -14,254 +13,44 @@ export default function CarroselDepoimentos() {
                     {/* Card */}
                     <article className={style.card}>
                         <img src="https://f.i.uol.com.br/fotografia/2023/08/25/169299396464e909ac19018_1692993964_3x2_md.jpg" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Jessíca Donato</h1>
-                            <div className={style.areaSpans}>
-                                <span>Medica</span>
-                                <span>47 anos</span>
-                            </div>
-                        </div>
                     </article>
                     {/* Card */}
                     <article className={style.card}>
                         <img src="https://www.cartacapital.com.br/wp-content/uploads/2019/11/cancerdemama-mulheres.jpg" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Dora Medeiros</h1>
-                            <div className={style.areaSpans}>
-                                <span>Professora</span>
-                                <span>47 anos</span>
-                            </div>
-                        </div>
                     </article>
                     {/* Card */}
                     <article className={style.card}>
                         <img src="https://assets-blog.pagseguro.uol.com.br/wp-content/2022/08/Auxilio-Taxista.jpg" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Jorge Medeiros</h1>
-                            <div className={style.areaSpans}>
-                                <span>Mecanico</span>
-                                <span>25 anos</span>
-                            </div>
-                        </div>
                     </article>
                     {/* Card */}
                     <article className={style.card}>
                         <img src="https://static.ndmais.com.br/2023/11/pedreiro-brincalhao-600x800.png" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Dona Maria do Socorro</h1>
-                            <div className={style.areaSpans}>
-                                <span>Designer</span>
-                                <span>47 anos</span>
-                            </div>
-                        </div>
                     </article>
                     
                     {/* Card */}
                     <article className={style.card}>
                         <img src="https://www.camara.leg.br/midias/image/2023/10/img20231026113029739-768x473.jpg" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Dona Maria do Socorro</h1>
-                            <div className={style.areaSpans}>
-                                <span>Designer</span>
-                                <span>47 anos</span>
-                            </div>
-                        </div>
                     </article>
 
                     {/* Card */}
                     <article className={style.card}>
                         <img src="https://f.i.uol.com.br/fotografia/2023/08/25/169299396464e909ac19018_1692993964_3x2_md.jpg" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Jessíca Donato</h1>
-                            <div className={style.areaSpans}>
-                                <span>Medica</span>
-                                <span>47 anos</span>
-                            </div>
-                        </div>
                     </article>
                     {/* Card */}
                     <article className={style.card}>
                         <img src="https://www.cartacapital.com.br/wp-content/uploads/2019/11/cancerdemama-mulheres.jpg" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Dora Medeiros</h1>
-                            <div className={style.areaSpans}>
-                                <span>Professora</span>
-                                <span>47 anos</span>
-                            </div>
-                        </div>
                     </article>
                     {/* Card */}
                     <article className={style.card}>
                         <img src="https://assets-blog.pagseguro.uol.com.br/wp-content/2022/08/Auxilio-Taxista.jpg" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Jorge Medeiros</h1>
-                            <div className={style.areaSpans}>
-                                <span>Mecanico</span>
-                                <span>25 anos</span>
-                            </div>
-                        </div>
                     </article>
                     {/* Card */}
                     <article className={style.card}>
                         <img src="https://static.ndmais.com.br/2023/11/pedreiro-brincalhao-600x800.png" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Dona Maria do Socorro</h1>
-                            <div className={style.areaSpans}>
-                                <span>Designer</span>
-                                <span>47 anos</span>
-                            </div>
-                        </div>
                     </article>
                     {/* Card */}
                     <article className={style.card}>
                         <img src="https://www.camara.leg.br/midias/image/2023/10/img20231026113029739-768x473.jpg" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Dona Maria do Socorro</h1>
-                            <div className={style.areaSpans}>
-                                <span>Designer</span>
-                                <span>47 anos</span>
-                            </div>
-                        </div>
-                    </article>
-
-                </div>
-            </div>
-
-            {/* Roll Container */}
-            <div className={`${style.rollContainer} ${style.roll2}`}>
-                {/* Slide Container */}
-                <div className={style.SlideContainer}>
-                    {/* Card */}
-                    <article className={style.card}>
-                        <img src="https://neon.com.br/aprenda/wp-content/uploads/2023/08/quanto-ganha-engenheiro-civil.png" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Jessíca Donato</h1>
-                            <div className={style.areaSpans}>
-                                <span>Medica</span>
-                                <span>47 anos</span>
-                            </div>
-                        </div>
-                    </article>
-                    {/* Card */}
-                    <article className={style.card}>
-                        <img src="https://www.acif.org.br/wp-content/uploads/2023/10/jovem-empreendedor-scaled-1.jpg" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Dora Medeiros</h1>
-                            <div className={style.areaSpans}>
-                                <span>Professora</span>
-                                <span>47 anos</span>
-                            </div>
-                        </div>
-                    </article>
-                    {/* Card */}
-                    <article className={style.card}>
-                        <img src="https://mediflix.com.br/wp-content/uploads/2020/03/7781-scaled-1.jpg" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Jorge Medeiros</h1>
-                            <div className={style.areaSpans}>
-                                <span>Mecanico</span>
-                                <span>25 anos</span>
-                            </div>
-                        </div>
-                    </article>
-                    {/* Card */}
-                    <article className={style.card}>
-                        <img src="https://goliveira.adv.br/wp-content/uploads/2022/04/Aposentadoria-especial-mecanico.jpg" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Dona Maria do Socorro</h1>
-                            <div className={style.areaSpans}>
-                                <span>Designer</span>
-                                <span>47 anos</span>
-                            </div>
-                        </div>
-                    </article>
-                    
-                    {/* Card */}
-                    <article className={style.card}>
-                        <img src="https://salario2024.com.br/wp-content/uploads/2023/05/domestic-routine-smiling-housewife-apron-standing-kitchen-scaled-e1685535865342.jpg" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Dona Maria do Socorro</h1>
-                            <div className={style.areaSpans}>
-                                <span>Designer</span>
-                                <span>47 anos</span>
-                            </div>
-                        </div>
-                    </article>
-
-                    {/* Card */}
-                    <article className={style.card}>
-                        <img src="https://neon.com.br/aprenda/wp-content/uploads/2023/08/quanto-ganha-engenheiro-civil.png" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Jessíca Donato</h1>
-                            <div className={style.areaSpans}>
-                                <span>Medica</span>
-                                <span>47 anos</span>
-                            </div>
-                        </div>
-                    </article>
-                    {/* Card */}
-                    <article className={style.card}>
-                        <img src="https://www.acif.org.br/wp-content/uploads/2023/10/jovem-empreendedor-scaled-1.jpg" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Dora Medeiros</h1>
-                            <div className={style.areaSpans}>
-                                <span>Professora</span>
-                                <span>47 anos</span>
-                            </div>
-                        </div>
-                    </article>
-                    {/* Card */}
-                    <article className={style.card}>
-                        <img src="https://mediflix.com.br/wp-content/uploads/2020/03/7781-scaled-1.jpg" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Jorge Medeiros</h1>
-                            <div className={style.areaSpans}>
-                                <span>Mecanico</span>
-                                <span>25 anos</span>
-                            </div>
-                        </div>
-                    </article>
-                    {/* Card */}
-                    <article className={style.card}>
-                        <img src="https://goliveira.adv.br/wp-content/uploads/2022/04/Aposentadoria-especial-mecanico.jpg" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Dona Maria do Socorro</h1>
-                            <div className={style.areaSpans}>
-                                <span>Designer</span>
-                                <span>47 anos</span>
-                            </div>
-                        </div>
-                    </article>
-                    {/* Card */}
-                    <article className={style.card}>
-                        <img src="https://salario2024.com.br/wp-content/uploads/2023/05/domestic-routine-smiling-housewife-apron-standing-kitchen-scaled-e1685535865342.jpg" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Dona Maria do Socorro</h1>
-                            <div className={style.areaSpans}>
-                                <span>Designer</span>
-                                <span>47 anos</span>
-                            </div>
-                        </div>
                     </article>
 
                 </div>
