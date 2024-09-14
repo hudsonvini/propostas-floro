b84fdb86b7de922ead3fa93c35ea8fdb27acdce6
Hudson <hudsonvini26@gmail.com>
2024-09-14 13:42:51 -0300

    add new style jingle button

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -52,6 +52,9 @@ export default function Home() {
 
             <h1>Nosso Jingle</h1>
             <a className={style.buttonJingle} href="">
+              <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M15 7.26795C16.3333 8.03775 16.3333 9.96225 15 10.7321L3 17.6603C1.66667 18.4301 1.01267e-06 17.4678 1.07997e-06 15.9282L1.68565e-06 2.0718C1.75295e-06 0.532196 1.66667 -0.430055 3 0.339746L15 7.26795Z" fill="#28B061"/>
+              </svg>
               Ouvir agora
             </a>
 
